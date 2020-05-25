43803dea4d5233db2e81250a42e002acfbe0f81b
w2cluster173@iiht.tech <w2cluster173@iiht.tech>
2020-05-25 21:31:42 +0530

    initial commit

---
+++ src/main/java/com/currency/convert/factor/microservice/currencyconversionfactorservice/CurrencyConversionFactorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.currency.convert.factor.microservice.currencyconversionfactorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionFactorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionFactorServiceApplication.class, args);
+	}
+
+}
